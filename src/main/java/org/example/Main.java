c01c805188bd69900f62045fa4be9f2109adf1e4
liza_chiornaia <blackliza270@gmail.com>
2023-12-03 19:32:26 +0300

    1 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        
+        String path = "C:\\Users\\Dima\\Desktop\\java_basics";
+        File file = new File(path);
+
+        System.out.println(getFolderSize(file));
+
+    }
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
